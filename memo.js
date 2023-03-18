5709770b040c285a7d752ab669b2f0f19f24f5a9
Maggie <maggiechang0319@gmail.com>
2023-03-18 18:19:09 +0800

    to do list

---
+++ memo.js
@@ -0,0 +1,48 @@
+let input = document.querySelector(".entered_list");
+let addBtn = document.querySelector(".add_list");
+let tasks = document.querySelector(".tasks");
+
+//Add btn disabled
+
+input.addEventListener("keyup", () => {
+  if (input.value.trim() != 0) {
+    addBtn.classList.add("active");
+  } else {
+    addBtn.classList.remove("active");
+  }
+});
+
+//Add new item to list
+
+addBtn.addEventListener("click", () => {
+  if (input.value.trim() != 0) {
+    let newItem = document.createElement("div");
+    newItem.classList.add("item");
+    newItem.innerHTML = `<p>${input.value}</p>
+      <div class="item_btn">
+       <i class="fa-solid fa-pen-to-square"></i>
+        <i class="fa-solid fa-xmark"></i>
+      </div>`;
+    tasks.appendChild(newItem);
+    input.value = "";
+  } else {
+    alert("Please enter a task");
+  }
+});
+
+// delete item from list
+
+tasks.addEventListener("click", (e) => {
+  console.log(e);
+  if (e.target.classList.contains("fa-xmark")) {
+    e.target.parentElement.parentElement.remove();
+  }
+});
+
+//mark item as complete
+
+tasks.addEventListener("click", (e) => {
+  if (e.target.classList.contains("fa-pen-to-square")) {
+    e.target.parentElement.parentElement.classList.toggle("completed");
+  }
+});
